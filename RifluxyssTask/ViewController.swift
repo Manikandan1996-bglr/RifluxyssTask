f9f0d90b5f53a7e51903d030fa77314a27bbb0d0
Manikandan <manikandan@velozion.com>
2020-12-09 16:20:20 +0530

    Initial Commit

---
+++ RifluxyssTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RifluxyssTask
+//
+//  Created by Velozion Mac on 03/July/2019A.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
